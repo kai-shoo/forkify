5296272f099a19347d1c89e081cb7c766adb680a
Kai Shoo <qsteamk@gmail.com>
2021-02-08 03:47:37 +0300

    builded mvc architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,8 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +15,27 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  // get id from hash
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // loading recipe
+    await model.loadRecipe(id);
+
+    // rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
